8effa6d605d9466feedaa0a30fc62bf0363155bc
elio75006 <elioraph38@gmail.com>
2021-02-04 19:20:59 +0100

    editing seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ require 'faker' #use faker gem to create random plausible names, cities, etc.
 50.times do 
 City.create!(name: Faker::Nation.capital_city)
 end
+
 100.times do
 Doctor.create!([{
   first_name: Faker::Name.first_name,
@@ -18,8 +19,6 @@ Doctor.create!([{
 }])
 end
 
-
-
 100.times do 
 Patient.create!([{
   first_name: Faker::Name.first_name,
@@ -29,6 +28,9 @@ Patient.create!([{
 }])
 end
 
+100.times do
+ Appointment.create!(date: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now), doctor_id: Doctor.all.sample.id, patient_id: Patient.all.sample.id, cities_id: City.all.sample.id) 
+end
 puts """
 #{Doctor.count} doctors created.
 #{City.count} cities created.
